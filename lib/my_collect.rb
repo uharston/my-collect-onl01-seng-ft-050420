18b495eb6648a70ff767edf6b1bd2a8f0d422430
Uriah Harston <uharston@gmail.com>
2020-05-11 00:39:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,5 @@
 def my_collect(array)
+<<<<<<< HEAD
   collection = []
   i = 0
   while i < array.length
@@ -7,3 +8,18 @@ def my_collect(array)
   end
   collection
 end
+=======
+  i = 0
+  while i > array.length
+    yield(array[i])
+    i += 1
+  end
+  array
+end
+
+my_collect(array) { |name| name.upcase }
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+>>>>>>> 09cc97165795b051b6fb6a708b0e3cc272dff5b5
